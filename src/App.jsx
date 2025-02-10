ec9349ea7aa9290d13737759c3eaa4a00c44fa4f
Prodigal <sujayprabhat@Prodigals-MacBook-Air.local>
2025-02-10 17:02:38 +0530

    account_management_webpage

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AccountDashboard from './components/AccountDashboard'
+
+function App() {
+  
+
+  return (<div>
+    <AccountDashboard />
+  </div>
+)
+
+}
+
+export default App
